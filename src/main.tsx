a6a05dc4009fb81172b07bd136aa0eb3e122d644
Abu Kalam <akwebdev69@gmail.com>
2025-08-23 13:19:42 +0600

    added seo meta title and descriptionfdfdfdfdf

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading.tsx";
 
 // Root কম্পোনেন্ট তৈরি
 const Root = () => {
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     // ব্যাকএন্ড থেকে অ্যাডমিন ডেটা ফেচ করা
+    setLoading(true);
     fetch(`${import.meta.env.VITE_BASE_URL}/admin_data`)
       .then((response) => response.json())
       .then((data) => {
@@ -14,24 +17,23 @@ const Root = () => {
         const metaTitle = document.getElementById("metaTitle");
         const metaDescription = document.getElementById("metaDescription");
         if (metaTitle) {
-          metaTitle.setAttribute(
-            "content",
-            data.websiteTitle || "Default Title"
-          );
+          metaTitle.setAttribute("content", data[0]?.websiteTitle);
         }
         if (metaDescription) {
-          metaDescription.setAttribute(
-            "content",
-            data.metaDescription || "Default Description"
-          );
+          metaDescription.setAttribute("content", data[0]?.metaDescription);
         }
         // ডকুমেন্ট টাইটেল আপডেট
-        document.title = data.websiteTitle || "Default Title";
+        document.title = data[0]?.websiteTitle;
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching admin data:", error);
+        setLoading(false);
       });
   }, []);
+  if (loading) {
+    return <Loading></Loading>;
+  }
 
   return <App />;
 };
